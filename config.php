e7b3b3f4c41d0bb489808c7d332081d30f8bd37e
Terence Lee <tlee.terence@gmail.com>
2015-10-21 13:14:34 -0700

    formatted readme, added example link

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $slackHostName = 'example';
 $slackAutoJoinChannels = 'example-C044KDT1F';
 $slackAuthToken = 'example-xoxs-1111111111-1111111111-1111111111-1111111111';
 
-$contactName = "[contact name]";
-$contactEmail = "example@example.com";
+$contactName = '[contact name]';
+$contactEmail = 'example@example.com';
 
 ?>
